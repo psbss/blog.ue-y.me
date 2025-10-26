0c9ceac21bf25c70467ea73442afa9e93882a82f
uetyo <uypsbss@gmail.com>
2025-10-26 23:59:20 +0900

    [Astro] Bio component (#219)

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Site metadata
+export const siteMetadata = {
+  title: 'MoreCuriosity',
+  author: '上ちょ / uetyo',
+  bioText: '都内で働くiOSエンジニア(4年目)',
+  description: '辛い時は筋トレで追い込むiOSエンジニアです',
+  social: {
+    x: 'psnzbss',
+    github: 'psbss',
+    speakerDeck: 'uetyo',
+  },
+};
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://blog.ue-y.me/',
+});
